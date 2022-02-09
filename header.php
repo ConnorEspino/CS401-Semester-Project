b45fc02c58267b6534d6afe1f0edad75ef5117e9
Connor Espino <espino.con@gmail.com>
2022-02-08 17:00:06 -0700

    Added PHP header

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>NFT Black Market</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <h1>
+        Howdy
+        </h1>
+    </head>
+    <body>
+        <div id="nav-bar">
+            <ul id="nav">
+                <li><a href="mine.php">Mine</a></li>
+                <li><a href="market.php">Market</a></li>
+                <li><a href="leaderboard.php">Leaderboard</a></li>
+            </ul>
+            <hr/>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'header.php';
+?>
+
+<html>
+    <body>
+        Howdy hey
+    </body>
+</html>
