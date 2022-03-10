f81375a380b5cfde73076d213e916227d2011dbe
Connor Espino <connorespino@u.boisestate.edu>
2022-03-10 15:13:42 -0700

    Mining problem test

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <html>
-    <?php require_once 'header.php';?>
+    <?php 
+        require_once 'header.php';
+        $operands = array("+", "-", "/", "*");
+    ?>
     
     <body>
         <div id="mine">
             <h1 id="mine_problem">
-                5*3
+                <?php
+                    $num1 = rand(-100, 100);
+                    $num2 = rand(-100, 100);
+                    $oper = rand(0,3);
+                    echo num1 . operands[oper] . num2;
+                ?>
             </h1>
 
             <div id="mine_textbox">
                 <form for="answer">
-                    <input type="text" id="answer" name="answer">
+                    <input type="text" id="answer" name="answer" value="Enter your answer...">
                 </form>
                 <button id="mine_button"> Enter </button>
             </div>
