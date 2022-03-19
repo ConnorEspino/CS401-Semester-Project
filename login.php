8ea7e69dae574e9477938c3b96869bafeb1494df
Connor Espino <connorespino@u.boisestate.edu>
2022-03-18 19:22:02 -0600

    Changes

--- login.php
+++ login.php
@@ -9,17 +9,17 @@
                 <div id="login_form">
                     <h1>Login</h1>
                     <input type="text" id="login_info" name="username_login" placeholder="Username">
-                    <input type="text" id="login_info" name="password_login" placeholder="Password">
+                    <input type="password" id="login_info" name="password_login" placeholder="Password">
                     <input type="submit" id="mine_button" value=" Enter ">
                 </div>
             </form>
 
-            <form for="register" action="register_handler.php" method="POST">
+            <form for="register" action="login_handler.php" method="POST">
 
                 <div id="login_form">
                     <h1>Register</h1>
                     <input type="text" id="login_info" name="username_reg" placeholder="Username">
-                    <input type="text" id="login_info" name="password_reg" placeholder="Password">
+                    <input type="password" id="login_info" name="password_reg" placeholder="Password">
                     <input type="submit" id="mine_button" value=" Enter ">
                 </div>
             </form>
