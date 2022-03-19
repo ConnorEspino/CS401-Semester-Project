e02e4623016e8c1fbe19b2fb545eaec9c6fd1974
Connor Espino <connorespino@u.boisestate.edu>
2022-03-18 19:26:05 -0600

    Bug tests

--- login_handler.php
+++ login_handler.php
@@ -3,17 +3,18 @@
 
     // $userLogin = $_POST['username_login'];
     // $passLogin = $_POST['password_login'];
+    echo $_POST;
 
-    $userReg = $_POST['username_reg'];
-    $passReg = $_POST['password_reg'];
+    // $userReg = $_POST['username_reg'];
+    // $passReg = $_POST['password_reg'];
 
-    require_once 'Dao.php';
-    $dao = new Dao();
+    // require_once 'Dao.php';
+    // $dao = new Dao();
 
-    if(isset($userReg)){
-        $dao->registerUser($userReg, $passReg);
-    }
+    // if(isset($userReg)){
+    //     $dao->registerUser($userReg, $passReg);
+    // }
 
-    header('Location: index.php');
-    exit;
+    // header('Location: index.php');
+    // exit;
 ?>
