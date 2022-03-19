64f58e9b9676fb8baf71248de1db0e9b9159ef7b
Connor Espino <connorespino@u.boisestate.edu>
2022-03-18 18:43:46 -0600

    Changes to layout of Login header

--- login.php
+++ login.php
@@ -3,14 +3,16 @@
     
     <body>
         <div id="login">
-            <h1>Login</h1>
+            
             <form for="login" action="login_handler.php" method="POST">
 
-            <div id="login_form">
-                <input type="text" id="username" name="username" placeholder="Username">
-                <input type="text" id="password" name="password" placeholder="Password">
-                <input type="submit" id="mine_button" value=" Enter ">
-            </div>
+                <div id="login_form">
+                    <h1>Login</h1>
+                    <input type="text" id="username" name="username" placeholder="Username">
+                    <input type="text" id="password" name="password" placeholder="Password">
+                    <input type="submit" id="mine_button" value=" Enter ">
+                </div>
+            </form>
         </div>  
             
     </body>
