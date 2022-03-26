ea621ecc01d40a207a884fcdad59980d850134b3
Connor Espino <connorespino@u.boisestate.edu>
2022-03-26 11:56:43 -0600

    added images directory

--- image_handler.php
+++ image_handler.php
@@ -10,8 +10,6 @@
 
     if (count($_FILES) > 0) {
         if ($_FILES["img"]["error"] > 0) {
-            header('Location: mine.php');
-            exit;
         } else {
             $imagePath = "/images/" . uniqid() . ".png";
             if (!move_uploaded_file($_FILES["img"]["tmp_name"], getcwd() . $imagePath)) {
