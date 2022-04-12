692c45ffeb41b4b2014c9af060102dda2c392c92
Connor Espino <connorespino@u.boisestate.edu>
2022-04-11 22:38:17 -0600

    Working on footer, and logout style Fix account when logged out

---
+++ footer.php
@@ -0,0 +1,17 @@
+<html>
+    <foot>
+        <title>NFT Black Market</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/x-icon" href="favicon.png">
+        
+        <div id="nav_bar">
+            <ul id="nav">
+                <li><a href="mining.php"><img src="Mine.png"></a></li>
+                <li><a href="market.php"><img src="Market.png"></a></li>
+                <li><a href="leaderboard.php"><img src="Leaderboard.png"></a></li>
+            </ul>
+            <a href="account.php"><img src="Account.png"></a>
+        </div>
+        <hr/>
+    </foot>
+</html>
--- header.php
+++ header.php
@@ -5,12 +5,19 @@
         <link rel="icon" type="image/x-icon" href="favicon.png">
         
         <div id="nav_bar">
-            <ul id="nav">
-                <li><a href="mining.php"><img src="Mine.png"></a></li>
-                <li><a href="market.php"><img src="Market.png"></a></li>
-                <li><a href="leaderboard.php"><img src="Leaderboard.png"></a></li>
-            </ul>
-            <a href="account.php"><img src="Account.png"></a>
+            <div id="nav">
+                <ul id="nav">
+                    <li><a href="mining.php"><img src="Mine.png"></a></li>
+                    <li><a href="market.php"><img src="Market.png"></a></li>
+                    <li><a href="leaderboard.php"><img src="Leaderboard.png"></a></li>
+                </ul>
+            </div>
+            <div id="account">
+                <a href="logout_handler.php">Log Out</a>
+                <a href="account.php"><img src="Account.png"></a>
+                
+            </div>
+
         </div>
         <hr/>
     </head>
